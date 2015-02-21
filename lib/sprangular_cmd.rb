fd50a719e6e6f60b5a1a7ea5d8b2473c05dfe581
Josh Nussbaum <joshnuss@gmail.com>
2015-02-20 19:54:32 -0500

    Added binary command

--- lib/sprangular_cmd.rb
+++ lib/sprangular_cmd.rb
@@ -1,5 +1,25 @@
 require "sprangular_cmd/version"
+require 'thor'
+require 'thor/group'
 
-module SprangularCmd
-  # Your code goes here...
+case ARGV.first
+  when nil, 'help', '-h', '--help', '-?'
+    puts <<-EOS
+sprangular - a tool for creating sprangular gems
+
+version:\t display version (alias: -v, --version)
+help:\t\t display help (alias: -h, --help)
+extension:\t create an extension (arguments: <extension-name>)
+install:\t install sprangular (arguments: <site-name>)
+EOS
+  when 'version', '-v', '--version'
+    puts SprangularCmd::VERSION
+  when 'extension'
+    ARGV.shift
+    require 'sprangular_cmd/extension'
+    SprangularCmd::Extension.start
+  else
+    ARGV.shift
+    require 'sprangular_cmd/installer'
+    SprangularCmd::Installer.start
 end
--- sprangular_cmd.gemspec
+++ sprangular_cmd.gemspec
@@ -1,7 +1,5 @@
 # coding: utf-8
-lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'sprangular_cmd/version'
+require_relative 'lib/sprangular_cmd/version'
 
 Gem::Specification.new do |spec|
   spec.name          = "sprangular_cmd"
